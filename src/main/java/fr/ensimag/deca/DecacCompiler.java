6cd5209b0f97fb7612daf0b781497aad7259f188
Arthur Hagenburg <Arthur.Hagenburg@grenoble-inp.org>
2022-01-06 17:01:34 +0100

    Mise en commentaire de l'étape B dans DecaCompiler et correction de CompilerOptions (Hello world fini)

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -125,9 +125,11 @@ public class DecacCompiler {
      */
     public boolean compile() {
         String sourceFile = source.getAbsolutePath();
-        String destFile = null;
         // A FAIRE: calculer le nom du fichier .ass à partir du nom du
         // A FAIRE: fichier .deca.
+        String destFile = sourceFile.substring(0, sourceFile.length()-4);
+        destFile += "ass";
+        //
         PrintStream err = System.err;
         PrintStream out = System.out;
         LOG.debug("Compiling file " + sourceFile + " to assembly file " + destFile);
@@ -179,8 +181,8 @@ public class DecacCompiler {
         assert(prog.checkAllLocations());
 
 
-        prog.verifyProgram(this);
-        assert(prog.checkAllDecorations());
+        // prog.verifyProgram(this);
+        // assert(prog.checkAllDecorations());
 
         addComment("start main program");
         prog.codeGenProgram(this);
