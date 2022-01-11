6caa09c904f0a124e09aa8f1a67589151f0cb969
Oscar Maggiori <oscar.maggiori@grenoble-inp.org>
2022-01-11 17:32:17 +0100

    Ajout de la méthode codeGenInst

--- src/main/java/fr/ensimag/deca/tree/IntLiteral.java
+++ src/main/java/fr/ensimag/deca/tree/IntLiteral.java
@@ -7,6 +7,8 @@ import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+
 import java.io.PrintStream;
 
 /**
@@ -29,7 +31,6 @@ public class IntLiteral extends AbstractExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-                this.setType
         Type type = new IntType(compiler.getSymbolTable().create("int"));
         this.setType(type);
         return type;
@@ -55,6 +56,11 @@ public class IntLiteral extends AbstractExpr {
     @Override
     protected void prettyPrintChildren(PrintStream s, String prefix) {
         // leaf node => nothing to do
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        compiler.addInstruction(new LOAD(value, compiler.getMemory().getLastRegister()));;
     }
 
 }
