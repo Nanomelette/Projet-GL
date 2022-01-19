e61e91de0909e4e3fac2808a29f6a0bdd70a7fd5
Arthur Hagenburg <Arthur.Hagenburg@grenoble-inp.org>
2022-01-19 10:56:47 +0100

    Implémentation de new

--- src/main/java/fr/ensimag/deca/tree/New.java
+++ src/main/java/fr/ensimag/deca/tree/New.java
@@ -3,12 +3,24 @@ package fr.ensimag.deca.tree;
 import java.io.PrintStream;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.Data;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
-
+import fr.ensimag.ima.pseudocode.DAddr;
+import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.BOV;
+import fr.ensimag.ima.pseudocode.instructions.BSR;
+import fr.ensimag.ima.pseudocode.instructions.LEA;
+import fr.ensimag.ima.pseudocode.instructions.NEW;
+import fr.ensimag.ima.pseudocode.instructions.POP;
+import fr.ensimag.ima.pseudocode.instructions.PUSH;
+import fr.ensimag.ima.pseudocode.instructions.STORE;
 public class New extends AbstractExpr {
 
     private Identifier class_;
@@ -39,4 +51,19 @@ public class New extends AbstractExpr {
         
     }
     
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        Label tas_plein = new Label("tas_plein");
+        Data data = compiler.getData();
+        GPRegister register = data.getFreeRegister(compiler);
+        compiler.addInstruction(new NEW(class_.getClassDefinition().getNumberOfFields(), register));
+        compiler.addInstruction(new BOV(tas_plein));
+        DAddr addr = (DAddr) getDVal(); // TODO : Adresse de la table des methodes de A et non getDVal
+        compiler.addInstruction(new LEA(addr, Register.R0));
+        compiler.addInstruction(new STORE(Register.R0, new RegisterOffset(0, register)));
+        compiler.addInstruction(new PUSH(register));
+        compiler.addInstruction(new BSR(new Label("init."+class_.getName())));
+        compiler.addInstruction(new POP(register));
+        data.setLastUsedRegister(register);
+    }
 }
