c99150066916d2dcbb1d5d31be2ac48e494ee2e2
Oscar Maggiori <oscar.maggiori@grenoble-inp.org>
2022-01-24 11:25:13 +0100

    Opti du codegenInst

--- src/main/java/fr/ensimag/deca/tree/MethodCall.java
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -105,16 +105,19 @@ public class MethodCall extends AbstractExpr {
 
         // On reserve la place pour les parametres
         compiler.addInstruction(new ADDSP(param.size() + 1));
-        GPRegister register = data.getFreeRegister(compiler);
+        // GPRegister register = data.getFreeRegister(compiler);
         // data.decrementFreeStoragePointer();
-
+        
         // On empile le parametre implicite
-        compiler.addInstruction(new LOAD(addresseClassRegister, register));
-        compiler.addInstruction(new STORE(register, new RegisterOffset(0, Register.SP)));
-
+        // compiler.addInstruction(new LOAD(addresseClassRegister, register));
+        // compiler.addInstruction(new STORE(register, new RegisterOffset(0, Register.SP)));
+        // Ajout
+        compiler.addInstruction(new STORE(addresseClassRegister, new RegisterOffset(0, Register.SP)));
+        
         // On empile les autres parametres
         param.codeGenListExpr(compiler);
         
+        GPRegister register = data.getFreeRegister(compiler);
         // register = data.getFreeRegister(compiler);
         compiler.addInstruction(new LOAD(new RegisterOffset(0, Register.SP), register));
         if (!(compiler.getCompilerOptions().getNoCheck())) {
